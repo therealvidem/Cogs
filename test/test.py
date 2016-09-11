2fb2ec74d8a09037c4a9cb56c91e5a767bdc1fa6
therealvidem <superninario@gmail.com>
2016-09-11 14:54:54 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 
@@ -5,10 +6,12 @@ class test:
 	def __init__(self, bot):
 		self.bot = bot
 		self.base = 'data/test/images/'
+		
+	pearlimages = ["{}pearl1.png", "{}pearl2.png", "{}pearl3.png"]
 	
 	@commands.command(pass_context=True)
 	async def pearl(self, context):
-		await self.bot.send_file(context.message.channel, '{}pearl.png'.format(self.base))
+		await self.bot.send_file(context.message.channel, random.choice(pearlimages).format(self.base))
 
 def setup(bot):
 	n = test(bot)

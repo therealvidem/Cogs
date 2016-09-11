2a2bbe837a89af8bfa8493bc840a756142f0b434
therealvidem <superninario@gmail.com>
2016-09-11 14:29:33 -0700

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+class test:
+	def __init__(self, bot):
+		self.bot = bot
+		self.base = 'data/downloader/videmcogs/test/data/images/'
+
+	@commands.command(pass_context=True)
+	async def pearl(self, context):
+	   await self.bot.send_file(context.message.channel, '{}pearl.png'.format(self.base))
+
+def setup(bot):
+	n = test(bot)
+bot.add_cog(n)

c61c96c5af07135d8eb0a961674e811c5c182000
therealvidem <superninario@gmail.com>
2016-09-13 22:43:26 -0700

    Create binary.py

---
+++ binary/binary.py
@@ -0,0 +1,26 @@
+import binascii
+import discord
+from discord.ext import commands
+
+class binary:
+	def __init__(self, bot):
+		self.bot = bot
+	
+	@commands.command(pass_context=True)
+	async def binarytoascii(self, context, message):
+		try:
+			await self.bot.say(bin(message, 16))
+		except:
+			await self.bot.say("Either you entered something invalid, or I fucked up.")
+			
+	@commands.command(pass_context=True)
+	async def asciitobinary(self, context, message):
+		try:
+		  n = int(message, 2)
+			await self.bot.say(binascii.unhexlify('%x' % n))
+		except:
+			await self.bot.say("Either you entered something invalid, or I fucked up.")
+
+def setup(bot):
+	n = binary(bot)
+  bot.add_cog(n)

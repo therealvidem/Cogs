c3787080fd56fd0c05a1d632e75e2eff608c7d74
therealvidem <superninario@gmail.com>
2016-09-13 22:58:37 -0700

    Update binary.py

--- binary/binary.py
+++ binary/binary.py
@@ -9,16 +9,15 @@ class binary:
 	@commands.command(pass_context=True)
 	async def binarytoascii(self, *, message):
 		try:
-			await self.bot.say(bin(int(message), 16))
+			await self.bot.say(bin(int(binascii.hexlify(message), 16)))
 		except:
-			await self.bot.say("Your message: " + message)
 			await self.bot.say("Either you entered something invalid, or I fucked up.")
 			
 	@commands.command(pass_context=True)
 	async def asciitobinary(self, context, message):
 		try:
 			n = int(message, 2)
-			await self.bot.say(binascii.unhexlify('%x' % n))
+			await self.bot.say(str(binascii.unhexlify('%x' % n)))
 		except:
 			await self.bot.say("Either you entered something invalid, or I fucked up.")
 

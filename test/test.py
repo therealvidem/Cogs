ad2b814cc30d7d763b8f999f0a92120112f0c6a4
therealvidem <superninario@gmail.com>
2016-10-21 16:46:08 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -35,12 +35,12 @@ class test:
 	
 	@commands.command(pass_context=True, invoke_without_command=True)
 	async def pearl(self, context, message):
-		await self.bot.send_file(context.message.channel, '{}meme_(' + str(random.randint(141)) + ').png'.format(self.base))
+		await self.bot.send_file(context.message.channel, '{}meme_(' + str(random.randint(1, 141)) + ').png'.format(self.base))
 			
 	@commands.command(pass_context=True, invoke_without_command=True)
 	async def bar(self, context, message):
 		await self.bot.say('{}meme (' + str(random.randint(27)) + ').png')
-		await self.bot.send_file(context.message.channel, '{}meme (' + str(random.randint(27)) + ').png'.format(self.base2))
+		await self.bot.send_file(context.message.channel, '{}meme (' + str(random.randint(1, 27)) + ').png'.format(self.base2))
 			
 	@commands.command(pass_context=True)
 	@checks.admin_or_permissions(move_members=True)

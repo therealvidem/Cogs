7802b18cad05e64de31566208480979cd6d12bf1
therealvidem <superninario@gmail.com>
2016-10-24 12:20:00 -0700

    Update color.py

--- color/color.py
+++ color/color.py
@@ -10,13 +10,13 @@ class color:
 	@commands.command(pass_context=True)
 	async def hexcolor(self, context, colr):
 		colr.replace('#', '')
-        	file = urllib2.urlopen('http://www.colourlovers.com/api/color/' + colr)
-        	data = file.read()
-        	file.close()
-        	data = xmltodict.parse(data)
-        	table = render_to_response('my_template.html', {'data': data})
-        	img = table['colors']['color']['imageUrl']
-        	await self.bot.say(img)
+		file = urllib2.urlopen('http://www.colourlovers.com/api/color/' + colr)
+		data = file.read()
+		file.close()
+		data = xmltodict.parse(data)
+		table = render_to_response('my_template.html', {'data': data})
+		img = table['colors']['color']['imageUrl']
+		await self.bot.say(img)
 
 def setup(bot):
 	n = color(bot)

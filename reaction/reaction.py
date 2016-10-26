f9f184d257d415d04e0342515969db0c5bf2bb50
therealvidem <superninario@gmail.com>
2016-10-25 19:06:46 -0700

    Update reaction.py

--- reaction/reaction.py
+++ reaction/reaction.py
@@ -3,3 +3,15 @@ from discord.ext import commands
 import asyncio
 
 class reaction:
+	def __init__(self, bot):
+		self.bot = bot
+		
+	async def listener(self, message):
+		if message.author.id != self.bot.user.id:
+			if message.content.lower() == 'sara':
+				await self.bot.send_message(message.channel, 'Ew!')
+				
+def setup(bot):
+	n = reaction(bot)
+	bot.add_listener(n.listener, "on_message")
+	bot.add_cog(n)

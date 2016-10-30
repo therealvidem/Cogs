51b067795e0df338b47de2c941151ba3d9912a68
therealvidem <superninario@gmail.com>
2016-10-30 15:54:55 -0700

    Update reaction.py

--- reaction/reaction.py
+++ reaction/reaction.py
@@ -5,23 +5,40 @@ import asyncio
 class reaction:
 	def __init__(self, bot):
 		self.bot = bot
+		self.listensub = {
+			'sara': 'Ew!',
+			'omg': '0 milligrams?',
+			'0mg': 'Oh my god?',
+			'syrz': 'that was one of them sam constantly changed itt'
+		}
+		self.listenstart = {
+			'hayy': '¡Harambe!',
+			'japenese': 'I\'m sorry, I dont\'t speak Japanese.',
+			'woah': 'whoa*,  you uneducated swine.'
+		}
 		
 	async def listener(self, message):
 		if message.author.id != self.bot.user.id:
-			if message.content[0:4].lower() == 'sara':
-				await self.bot.send_message(message.channel, 'Ew!')
-			elif message.content[0:3].lower() == 'omg':
-				await self.bot.send_message(message.channel, '0 milligrams?')
-			elif message.content[0:3].lower() == '0mg':
-				await self.bot.send_message(message.channel, 'Oh my god?')
-			elif message.content[0:4].lower() == 'syrz':
-				await self.bot.send_message(message.channel, 'that was one of them sam constantly changed itt')
-			if message.content.lower().startswith('hayy') or message.content.lower().startswith('haayy'):
-				await self.bot.send_message(message.channel, '¡Harambe!')
-			elif message.content.lower().startswith('japanese'):
-				await self.bot.send_message(message.channel, 'I\'m sorry, I don\'t speak Japanese.')
-			elif message.content.lower().startswith('woah'):
-				await self.bot.send_message(message.channel, 'whoa*,  you uneducated swine.')
+			for k, v in self.listensub:
+				if message.content[0:len(k)].lower() == k:
+					await self.bot.send_message(message.channel, v)
+			for k, v in self.listenstart:
+				if message.content.lower().startswith(k):
+					await self.bot.send_message(message.channel, v)
+			# if message.content[0:4].lower() == 'sara':
+			#	await self.bot.send_message(message.channel, 'Ew!')
+			# elif message.content[0:3].lower() == 'omg':
+			#	await self.bot.send_message(message.channel, '0 milligrams?')
+			# elif message.content[0:3].lower() == '0mg':
+			#	await self.bot.send_message(message.channel, 'Oh my god?')
+			# elif message.content[0:4].lower() == 'syrz':
+			#	await self.bot.send_message(message.channel, 'that was one of them sam constantly changed itt')
+			# if message.content.lower().startswith('hayy') or message.content.lower().startswith('haayy'):
+			#	await self.bot.send_message(message.channel, '¡Harambe!')
+			# elif message.content.lower().startswith('japanese'):
+			#	await self.bot.send_message(message.channel, 'I\'m sorry, I don\'t speak Japanese.')
+			# elif message.content.lower().startswith('woah'):
+			#	await self.bot.send_message(message.channel, 'whoa*,  you uneducated swine.')
 				
 def setup(bot):
 	n = reaction(bot)

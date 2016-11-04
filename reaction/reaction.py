8a305ba5a11380b8cfaeff3bdb915202e6ece44e
therealvidem <superninario@gmail.com>
2016-11-04 15:25:58 -0700

    Update reaction.py

--- reaction/reaction.py
+++ reaction/reaction.py
@@ -5,27 +5,28 @@ import asyncio
 class reaction:
 	def __init__(self, bot):
 		self.bot = bot
-		self.mentionpass = ''
 		self.listensub = {
 			'sara': 'Ew!',
 			'omg': '0 milligrams?',
 			'0mg': 'Oh my god?',
 			'syrz': 'It\'s called a dress.',
-			'ppap': '/ban ' + self.mentionpass
+			'bob': 'http://66.media.tumblr.com/5756bdebeb6a5e675ee45d68b6c09096/tumblr_ntt4qzMvQW1rz6w0do1_500.gif',
+			'ppap': '/ban '
 		}
 		self.listenstart = {
 			'hayy': '¡Harambe!',
 			'japanese': 'I\'m sorry, I dont\'t speak Japanese.',
-			'woah': 'whoa*,  you uneducated swine.',
-			'bob': 'http://66.media.tumblr.com/5756bdebeb6a5e675ee45d68b6c09096/tumblr_ntt4qzMvQW1rz6w0do1_500.gif'
+			'woah': 'whoa*,  you uneducated swine.'
 		}
 		
 	async def listener(self, message):
 		if message.author.id != self.bot.user.id:
-			self.mentionpass = message.author.username
 			for k, v in self.listensub.items():
 				if message.content[0:len(k)].lower() == k:
-					await self.bot.send_message(message.channel, v)
+					if k == 'ppap':
+						await self.bot.send_message(message.channel, v + message.author.username)
+					else:
+						await self.bot.send_message(message.channel, v)
 			for k, v in self.listenstart.items():
 				if message.content.lower().startswith(k):
 					await self.bot.send_message(message.channel, v)

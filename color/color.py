bc638db9de8bdb6ed734427b89bf8d0c0f9fb7f3
therealvidem <superninario@gmail.com>
2016-11-08 09:40:00 -0800

    Update color.py

--- color/color.py
+++ color/color.py
@@ -5,7 +5,7 @@ import random
 class color:
 	def __init__(self, bot):
 		self.bot = bot
-		self.hexalphabet = {'A', 'B', 'C', 'D', 'E', 'F'}
+		self.hexalphabet = ['A', 'B', 'C', 'D', 'E', 'F']
 	
 	@commands.command(pass_context=True)
 	async def hexcolor(self, context, colr):
@@ -19,10 +19,16 @@ class color:
 		
 	@commands.command(pass_context=True)
 	async def randomcolor(self, context):
-		colr = rgbcol(random.randint(1, 3))
+		rand = random.randint(1, 3)
+		if rand == 1:
+			colr = 'F*0000'
+		elif rand == 2:
+			colr = '00F*00'
+		else:
+			colr = '0000F*'
 		for x in range(0, 4):
 			colr.replace('0', random.randint(0,10), 1)
-		colr.replace('*', self.hexalphabet.get(random.randint(0, 6))
+		colr.replace('*', random.choice(self.hexalphabet))
 		await self.bot.say('http://www.colorhexa.com/' + colr + '.png')
 
 def rgbcol(x):

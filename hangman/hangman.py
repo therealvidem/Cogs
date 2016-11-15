7b91faf336096ca91f9a73e41040afe730a14c98
therealvidem <superninario@gmail.com>
2016-11-15 12:44:55 -0800

    Update hangman.py

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -30,43 +30,44 @@ class hangman:
 				self.guesses = []
 				self.guessword = '-' * (len(self.word) - 1)
 				await self.bot.say('The word is ' + self.guessword)
-			elif msg == 'end' and self.insession:
-				self.insession = False
-				await self.bot.say('Ended session')
-				await self.bot.say('The word was ' + self.word)
-			elif len(msg) == 1 and self.insession:
-				if msg in self.guesses:
-					await self.bot.say('You already guessed that.')
-					await self.bot.say('The word is ' + self.guessword)
-				elif self.word.find(msg) != -1:
-					await self.bot.say('The word does contain ' + msg)
-					for i, letter in enumerate(self.word, start = 1):
-						if letter == msg:
-							self.guessword = self.guessword[0:i - 1] + msg + self.guessword[i:len(self.guessword)]
-					self.guesses.append(msg)
-					self.guesses.sort()
-					self.numguesses = self.numguesses + 1
-					await self.bot.say('Guesses: [%s]' % ', '.join(map(str, self.guesses)))
-					if self.guessword.find('-') == -1:
-						await self.bot.say('You won with ' + str(self.numguesses) + ' guess(es)!')
-						await self.bot.say('The word was ' + self.word)
-						self.insession = False
+			elif self.insession:
+				if len(msg) == 1:
+					if msg in self.guesses:
+						await self.bot.say('You already guessed that.')
+						await self.bot.say('The word is ' + self.guessword)
+					elif self.word.find(msg) != -1:
+						await self.bot.say('The word does contain ' + msg)
+						for i, letter in enumerate(self.word, start = 1):
+							if letter == msg:
+								self.guessword = self.guessword[0:i - 1] + msg + self.guessword[i:len(self.guessword)]
+						self.guesses.append(msg)
+						self.guesses.sort()
+						self.numguesses = self.numguesses + 1
+						await self.bot.say('Guesses: [%s]' % ', '.join(map(str, self.guesses)))
+						if self.guessword.find('-') == -1:
+							await self.bot.say('You won with ' + str(self.numguesses) + ' guess(es)!')
+							await self.bot.say('The word was ' + self.word)
+							self.insession = False
+						else:
+							await self.bot.say('The word is ' + self.guessword) 
 					else:
-						await self.bot.say('The word is ' + self.guessword) 
-				else:
-					self.guesses.append(msg)
-					self.guesses.sort()
-					await self.bot.say('There are no ' + msg + '\'s')
-					await self.bot.say('Guesses: [%s]' % ', '.join(map(str, self.guesses)))
-					await self.bot.say('The word is ' + self.guessword)
-			elif msg == self.word and self.insession:
-				await self.bot.say('You won with ' + str(self.numguesses) + ' guess(es)!')
-				self.insession = False
-			elif msg != self.word and self.insession:
-				await self.bot.say('msg' + msg)
-				await self.bot.say('self.word' + self.word)
-				self.numguesses = self.numguesses + 1
-				await self.bot.say('It\'s not ' + msg)
+						self.guesses.append(msg)
+						self.guesses.sort()
+						await self.bot.say('There are no ' + msg + '\'s')
+						await self.bot.say('Guesses: [%s]' % ', '.join(map(str, self.guesses)))
+						await self.bot.say('The word is ' + self.guessword)
+				elif msg == 'end':
+					self.insession = False
+					await self.bot.say('Ended session')
+					await self.bot.say('The word was ' + self.word)
+				elif msg == self.word:
+					await self.bot.say('You won with ' + str(self.numguesses) + ' guess(es)!')
+					self.insession = False
+				elif msg != self.word:
+					await self.bot.say('msg' + msg)
+					await self.bot.say('self.word' + self.word)
+					self.numguesses = self.numguesses + 1
+					await self.bot.say('It\'s not ' + msg)
 
 					   
 def setup(bot):

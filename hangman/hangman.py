0fca36a7bf0bb5d13e755aced6dd830281457165
therealvidem <superninario@gmail.com>
2016-11-14 20:34:19 -0800

    Update hangman.py

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -28,11 +28,9 @@ class hangman:
 		elif msg == 'end' and self.insession:
 			self.insession = False
 			await self.bot.say('Ended session')
+			await self.bot.say('The word was ' + self.word)
 		elif msg == 'gusess' and self.insession:
 			await self.bot.say('[%s]' % ', '.join(map(str, self.guesses))
-		elif msg == 'giveup' and self.insession:
-			await self.bot.say('The word was ' + self.word)
-			self.insession = False
 		elif len(msg) == 1 and self.insession:
 			if msg in self.guesses:
 				await self.bot.say('You already guessed that.')

95e111ed3a128259e6796dede20bfc5833ed93c6
therealvidem <superninario@gmail.com>
2016-11-15 12:49:08 -0800

    Update hangman.py

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -60,7 +60,7 @@ class hangman:
 					self.insession = False
 					await self.bot.say('Ended session')
 					await self.bot.say('The word was ' + self.word)
-				elif msg[0:] == self.word[0:]:
+				elif str(msg) == str(self.word):
 					await self.bot.say('You won with ' + str(self.numguesses) + ' guess(es)!')
 					self.insession = False
 				elif msg != self.word:

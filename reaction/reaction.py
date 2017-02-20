e3be07240e994f64a61a9ed8935b1e037303a9d4
therealvidem <superninario@gmail.com>
2017-02-20 12:13:23 -0800

    Update reaction.py

--- reaction/reaction.py
+++ reaction/reaction.py
@@ -23,15 +23,16 @@ class reaction:
 		
 	async def listener(self, message):
 		if message.author.id != self.bot.user.id:
-			container = ''
-			for k, v in self.listensub.items():
-				if message.content[0:len(k)].lower() == k:
-					if k == 'ppap':
-						container = message.author.name + '*'
-					await self.bot.send_message(message.channel, v + container)
-			for k, v in self.listenstart.items():
-				if message.content.lower().find(k) != -1:
-					await self.bot.send_message(message.channel, v)
+			if self.bot.user.id == '224328344769003520':
+				container = ''
+				for k, v in self.listensub.items():
+					if message.content[0:len(k)].lower() == k:
+						if k == 'ppap':
+							container = message.author.name + '*'
+						await self.bot.send_message(message.channel, v + container)
+				for k, v in self.listenstart.items():
+					if message.content.lower().find(k) != -1:
+						await self.bot.send_message(message.channel, v)
 			if message.content == 'FORM THE WEIRD QUARTET!' and self.bot.user.id == '224328344769003520':
 				await asyncio.sleep(0.5)
 				await self.bot.send_message(message.channel, 'VIDEM!')

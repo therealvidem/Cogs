19ae845ff5da32cfa6b527c6600cfed753cec159
therealvidem <superninario@gmail.com>
2017-03-03 12:39:09 -0800

    Update choose.py

--- choose/choose.py
+++ choose/choose.py
@@ -9,16 +9,24 @@ class choose:
 
 	@commands.command(pass_context=True)
 	async def choosebetween(self, ctx, *args):
-		choices = args.items()
-		await self.bot.say(random.choice(choices))
+		choices = list(args)
+		if choices.len > 1:
+			await self.bot.say(random.choice(choices))
+		else:
+			await self.bot.say('Not enough choices to choose from.')
 		
 	@commands.command(pass_context=True)
 	async def chooserate(self, ctx, *args):
 		author = ctx.message.author
-		choices = args.items().shuffle()
-		em = discord.Embed(title='Choices', colour=0x2F93E0)
-		em.set_author(name=str(author), icon_url=author.avatar_url)
-		await client.send_message(ctx.message.channel, embed=em)
+		choices = list(args).shuffle()
+		if choices.len > 1:
+			em = discord.Embed(title='Choices', colour=0x2F93E0)
+			em.set_author(name=str(author), icon_url=author.avatar_url)
+			for x in range(1, choices.len):
+				em.add_field(name=x, value=choices[x], True)
+			await self.bot.send_message(ctx.message.channel, embed=em)
+		else:
+			await self.bot.say('Not enough choices to choose from')
 
 def setup(bot):
 	n = choose(bot)

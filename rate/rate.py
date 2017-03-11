143aa766dde055575879ebe98b5adfb1f4da6d80
therealvidem <superninario@gmail.com>
2017-03-10 23:29:31 -0800

    Update rate.py

--- rate/rate.py
+++ rate/rate.py
@@ -25,7 +25,8 @@ class rate:
             random.seed(thing.lower())
             rate = random.randint(0, 10)
             emoji = ':thumbsup:' if rate >= 5 else ':thumbsdown:'
-            await self.bot.say('I give {} a {}/10 {}'.format(thing, rate, emoji))
+            article = 'an' if rate == 8 else 'a'
+            await self.bot.say('I give **{}** {} **{}/10** {}'.format(thing, article, rate, emoji))
         else:
             await self.bot.say('Do \'{}rate thing\' for more information.'.format(context.prefix))
     
@@ -36,7 +37,8 @@ class rate:
             random.seed(name.lower())
             rate = random.randint(0, 10)
             emoji = ':thumbsup:' if rate >= 5 else ':thumbsdown:'
-            await self.bot.say('I give {} a {}/10 {}'.format(name, rate, emoji))
+            article = 'an' if rate == 8 else 'a'
+            await self.bot.say('I give **{}** {} **{}/10** {}'.format(name, article, rate, emoji))
         else:
             await self.bot.say('Do \'{}help rate someone\' for more information.'.format(context.prefix))
             
@@ -52,7 +54,7 @@ class rate:
             rate = random.randint(0, 10)
             emoji = ':heart:' if rate >= 5 else ':broken_heart:'
             article = 'an' if rate == 8 else 'a'
-            await self.bot.say('I give the {} {} {}/10 {}'.format(shipname, article, rate, emoji))
+            await self.bot.say('I give the **{}** {} **{}/10** {}'.format(shipname, article, rate, emoji))
         else:
             await self.bot.say('Do \'{}help rate ship\' for more information.'.format(context.prefix))
 

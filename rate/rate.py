395e3f119f122d02ea0e82a396c15c22658aa1a4
therealvidem <superninario@gmail.com>
2017-03-14 15:35:39 -0700

    Update rate.py

--- rate/rate.py
+++ rate/rate.py
@@ -60,13 +60,13 @@ class rate:
             
     @_rate.command(pass_context=True, name='list')
     async def _list(self, context, *args):
-        author = ctx.message.author
+        author = context.message.author
         choices = list(args)
         if len(choices) > 1:
             random.shuffle(choices)
             em = discord.Embed(title='Choices', colour=0x2F93E0)
             em.set_author(name=str(author), icon_url=author.avatar_url)
-            for x in range(1, len(choices)):
+            for x in range(0, len(choices)):
                 em.add_field(name=x, value=choices[x])
             await self.bot.send_message(ctx.message.channel, embed=em)
         else:

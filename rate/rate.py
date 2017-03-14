812b9cd1a7bd60548cf64ca3552c2b62e5bd4f9c
therealvidem <superninario@gmail.com>
2017-03-14 15:33:16 -0700

    Update rate.py

--- rate/rate.py
+++ rate/rate.py
@@ -57,6 +57,20 @@ class rate:
             await self.bot.say('I give the **{} x {}** {} **{}/10** {}'.format(name1, name2, article, rate, emoji))
         else:
             await self.bot.say('Do \'{}help rate ship\' for more information.'.format(context.prefix))
+            
+    @_rate.command(pass_context=True, name='list')
+    async def _list(self, context, args*):
+        author = ctx.message.author
+        choices = list(args)
+        if len(choices) > 1:
+            random.shuffle(choices)
+            em = discord.Embed(title='Choices', colour=0x2F93E0)
+            em.set_author(name=str(author), icon_url=author.avatar_url)
+            for x in range(1, len(choices)):
+                em.add_field(name=x, value=choices[x])
+            await self.bot.send_message(ctx.message.channel, embed=em)
+        else:
+            await self.bot.say('Not enough choices to choose from')
 
 def setup(bot):
     bot.add_cog(rate(bot))

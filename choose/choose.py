821c1f968beb3c698c935e5c84a8abca2a63d636
therealvidem <superninario@gmail.com>
2017-03-14 15:53:37 -0700

    Update choose.py

--- choose/choose.py
+++ choose/choose.py
@@ -14,20 +14,6 @@ class choose:
 			await self.bot.say(random.choice(choices))
 		else:
 			await self.bot.say('Not enough choices to choose from.')
-		
-	@commands.command(pass_context=True)
-	async def chooserate(self, ctx, *args):
-		author = ctx.message.author
-		choices = list(args)
-		if len(choices) > 1:
-			random.shuffle(choices)
-			em = discord.Embed(title='Choices', colour=0x2F93E0)
-			em.set_author(name=str(author), icon_url=author.avatar_url)
-			for x in range(1, len(choices)):
-				em.add_field(name=x, value=choices[x])
-			await self.bot.send_message(ctx.message.channel, embed=em)
-		else:
-			await self.bot.say('Not enough choices to choose from')
 
 def setup(bot):
 	n = choose(bot)

2b3d834fe26a441992b7db11cc5d4721e934b9b9
therealvidem <superninario@gmail.com>
2017-03-22 18:13:45 -0700

    Update rate.py

--- rate/rate.py
+++ rate/rate.py
@@ -52,12 +52,11 @@ class rate:
             name2 = str(member2)
             for person in context.message.server.members:
                 if member1 == person.mention or member1 == person.name:
-                    name1 = person.id
+                    name1 = person.name
                     member1 = str(person)
                 if member2 == person.mention or member2 == person.name:
-                    name2 = person.id
+                    name2 = person.name
                     member2 = str(person)
-            await self.bot.say('m1: ' + member1 + ' m2: ' + member2)
             shiplist = [member1.lower(), member2.lower()]
             shiplist.sort()
             shipname = ' x '.join(shiplist)

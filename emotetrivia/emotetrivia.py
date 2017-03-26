e7a23d0a163b1c803f7664bd3455555350a4d51a
therealvidem <superninario@gmail.com>
2017-03-26 13:07:56 -0700

    Update emotetrivia.py

--- emotetrivia/emotetrivia.py
+++ emotetrivia/emotetrivia.py
@@ -109,7 +109,7 @@ class EmoteTrivia:
             await self.bot.say("There are no trivia lists available.")
             
     async def test(self, reaction, user):
-        await self.bot.say(reaction.emoji.name)
+        await self.bot.say(reaction.message)
 
 class TriviaSession():
     def __init__(self, message, settings):

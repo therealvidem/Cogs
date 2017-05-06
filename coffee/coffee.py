58449c52962b01c24782b25305685ed49b138ce7
therealvidem <superninario@gmail.com>
2017-05-06 16:52:33 -0700

    Update coffee.py

--- coffee/coffee.py
+++ coffee/coffee.py
@@ -14,7 +14,7 @@ class coffee:
         if context.invoked_subcommand is None:
             if context.message.server.id not in self.coffee:
                 self.coffee[context.message.server.id] = {}
-                await self.bot.say('Do {}help coffee.'.format(context.prefix)) 
+            await self.bot.say('Do {}help coffee.'.format(context.prefix)) 
 
     @_coffee.command(pass_context=True, name='plus')
     @checks.admin()

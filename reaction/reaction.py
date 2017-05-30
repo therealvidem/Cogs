91de10e65163a8caf2010738346201c36e8d6f97
therealvidem <superninario@gmail.com>
2017-05-29 19:31:31 -0700

    Update reaction.py

--- reaction/reaction.py
+++ reaction/reaction.py
@@ -64,11 +64,10 @@ class reaction:
 	async def voicelistener(self, before, after):
 		if before.voice.voice_channel is None and after.voice.voice_channel:
 			channel = [c for c in after.server.channels if c.id == '132586673383931904']
-			await self.bot.send_message(channel, 'I predict that ' + after.nick + ' will join the voice channel.')
+			await self.bot.send_message(channel, 'I predict that ' + after.name + ' will join the voice channel.')
 		elif before.voice.voice_channel and after.voice.voice_channel is None:
-			print(after.nick)
 			channel = [c for c in after.server.channels if c.id == '132586673383931904']
-			await self.bot.send_message(channel, 'I foresee that ' + after.nick + ' will leave the voice channel.')
+			await self.bot.send_message(channel, 'I foresee that ' + after.name + ' will leave the voice channel.')
 				
 def setup(bot):
 	n = reaction(bot)

78753c186b5ea59a9f1084cdf861adb842803566
therealvidem <superninario@gmail.com>
2017-06-11 14:56:11 -0700

    Create test.py

--- test/test.py
+++ test/test.py
@@ -279,6 +279,21 @@ class test:
 				await self.bot.say('wat')
 				
 	@commands.command(pass_context=True)
+	async def ophiuchus(self, context, n: int=10):
+		emote = ':ophiuchus:'
+		if n and n > 0 and n * len(emote) < 2000:
+			await self.bot.say(emote * n)
+		else:
+			if n > 2000 - len(emote):
+				await self.bot.say('You\'re a bit too happy there, bud.')
+			elif n == -1:
+				await self.bot.say(emote * 280)
+			elif n < -1:
+				await self.bot.say('Are you silent?')
+			else:
+				await self.bot.say('wat')
+				
+	@commands.command(pass_context=True)
 	async def ae(self, context, n: int=10):
 		chance = random.randint(1, 2)
 		begintext = 'VA'

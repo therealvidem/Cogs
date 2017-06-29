9a5f14ad5ac853b138e86e555b8d0c5b56cec528
therealvidem <superninario@gmail.com>
2017-06-29 14:21:01 -0700

    Fixed a lot of things in test

--- test/test.py
+++ test/test.py
@@ -21,6 +21,12 @@ import re
 from .utils.dataIO import dataIO
 
 class test:
+    memetypes = [
+        "p"
+        "m"
+        "b"
+    ]
+
     def __init__(self, bot):
         self.bot = bot
         self.base = 'data/test/images/'
@@ -29,13 +35,30 @@ class test:
         self.stabbingobjects = dataIO.load_json('data/test/stabbingobjects.json')
         self.coffee = dataIO.load_json('data/test/coffee.json')
         self.memes = dataIO.load_json('data/test/memes.json')
+        self.quotes = dataIO.load_json('data/test/quotes.json')
         self.counting = False
 
-    @commands.command(pass_context=True, invoke_without_command=True)
-    async def memecount(self, context, memetype):
+    # @commands.group()
+    # async def emote(self, context, memetype, num):
+    #     if memetype and memetype in memetypes:
+    #         memenum = random.randint(1, self.memes[memetype])
+    #         if ()
+    #     elif memetype and memetype not in memetypes:
+    #         await self.bot.say('{} isn\'t a meme type I recognize from my stash!'.format(memetype))
+
+    @commands.group()
+    async def quote(self, context, author: discord.Member, quote):
+        self.quotes[author] = quote
+        dataIO.save_json('data/test/quotes.json', self.quotes)
+        await self.bot.delete_message(context.message)
+        await self.bot.say('"' + quote + '"\n -' + author.name + '\n' + datetime.now().year)
+
+    @commands.command()
+    async def emote(self, context, memetype):
         await self.bot.say(memetype + ' has ' + str(self.memes[memetype]) + ' emotes.')
 
-    @commands.command(pass_context=True, invoke_without_command=True)
+    @commands.command()
+    @commands.cooldown(1, 3)
     async def pearl(self, context, num: str = None):
         memenum = random.randint(1, self.memes['vp'])
         try:
@@ -43,7 +66,8 @@ class test:
         except:
             await self.bot.send_file(context.message.channel, self.base + 'meme (' + str(memenum) + ').png')
 
-    @commands.command(pass_context=True, invoke_without_command=True)
+    @commands.command()
+    @commands.cooldown(1, 3)
     async def bar(self, context, num: str = None):
         if discord.utils.get(context.message.server.roles, name='BAR'):
             memenum = random.randint(1, self.memes['vb'])
@@ -52,7 +76,8 @@ class test:
             except:
                 await self.bot.send_file(context.message.channel, self.base2 + 'meme (' + str(memenum) + ').png')
 
-    @commands.command(pass_context=True, invoke_without_command=True)
+    @commands.command()
+    @commands.cooldown(1, 3)
     async def meme(self, context, num: str = None):
         if discord.utils.get(context.message.server.roles, name='BAR'):
             memenum = random.randint(1, self.memes['vm'])
@@ -61,7 +86,7 @@ class test:
             except:
                 await self.bot.send_file(context.message.channel, self.base3 + 'meme (' + str(memenum) + ').png')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def addvp(self, context, url):
         if discord.utils.get(context.message.server.roles, name='BAR'):
             r = requests.get(url, stream=True)
@@ -76,7 +101,7 @@ class test:
             else:
                 await self.bot.say('Unable to add emote.')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def addvb(self, context, url):
         if discord.utils.get(context.message.server.roles, name='BAR'):
             r = requests.get(url, stream=True)
@@ -91,7 +116,7 @@ class test:
             else:
                 await self.bot.say('Unable to add emote.')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def addvm(self, context, url):
         if discord.utils.get(context.message.server.roles, name='BAR'):
             r = requests.get(url, stream=True)
@@ -106,12 +131,12 @@ class test:
             else:
                 await self.bot.say('Unable to add emote.')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def rollbetween(self, context, one, two):
         choice = random.randint(int(one), int(two))
         await self.bot.say('You got a ' + str(choice) + '.')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def factorial(self, context, n: int = None):
         if n is None:
             await self.bot.say('wat')
@@ -120,7 +145,7 @@ class test:
         elif n < 0:
             await self.bot.say("Are you trying to find the factorial of a negative number? You're batshit crazy, lad!")
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def permutations(self, context, n: int = None, r: int = None):
         if n is None or r is None:
             await self.bot.say('wat')
@@ -129,7 +154,7 @@ class test:
         elif n < 0 or r < 0:
             await self.bot.say('I dunno what the means.')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def combinations(self, context, n: int = None, r: int = None):
         if n is None or r is None:
             await self.bot.say('wat')
@@ -138,17 +163,19 @@ class test:
         elif n < 0 or r < 0:
             await self.bot.say('I dunno what the means.')
 
-    @commands.command(pass_context=True, name='invite')
-    async def _invite(self, context, botid: str = None, server: str = None):
+    @commands.command()
+    async def invite(self, context, botid: str = None, server: str = None):
         if botid:
             await self.bot.say(discord.utils.oauth_url(botid, permissions=None, server=server, redirect_uri=None))
 
-    @commands.command(pass_context=True)
+    @commands.command()
+    @commands.cooldown(1, 5)
     async def sauce(self, context):
         await self.bot.say(
             '***H̭̓͗̏̅͘E̳̰̠͖͓͕͊͋ͯͭ̿̔Ÿ͓̜͎̪͉́͆ͮ̇́̆̊̒̈͝ ̷̧̖̌ͮ̉̂̿ͪ͗̔V̶̯̩̤̥ͧ͊̋͊ͧ͞S̴̷̳͈̓͗̽̏̇A̶͎͈̔̍ͨ̉̚͞U̼̻͍̬̪̦ͦ͌ͩ̑͋͊̈́ͅC̺̻̪̯̗̖̖͂ͪ̈́̕Ȩ̮̤̫̯̟ͭ͌̅̒̄͘͠ͅ,̗͖̯͙͖̮̪̏́ ̶̨̫̮͎̗̣͋̍̓͟M̐͂͞͏̘̥͎̕I̴̭͉̰͎͈͎͔̗̭ͥ͒͗̊́͘C̷̗̬͙͎̠͇͊̔ͦ̆͠H̡̋͐͗́̚͝҉̫̣̳̦̥̮̗̜ͅĂ͓̹͙̽ͨ̑̋̈́̚͘͠E̢̺̘̳̬͙̅ͪͮ͒͑̒͒̇͂͞L͔̣̟̗͉̹̾͊̈́͋ͭ̑ͥ̕ ͬͯ҉̡̨͓̝̗̺H̰͕͌ͨĘ̳̟͕̹̘̠͇͎́̄͑̀ͅR͈̳ͬ̉Eͣ͞҉̳̘̦͉̞̣***')
 
-    @commands.command(pass_context=True)
+    @commands.command()
+    @commands.cooldown(5, 3)
     async def stab(self, context, *, member: discord.Member = None):
         obj = random.choice(self.stabbingobjects['objects'])
         word = random.choice(
@@ -156,7 +183,7 @@ class test:
              "prods"])
         await self.bot.say('{0} {1} {2} with {3}.'.format(context.message.author.name, word, member.mention, obj))
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def pat(self, context, member: discord.Member = None, n: int = 3):
         member = member or context.message.author
         if n and n <= 650 and n > 0:
@@ -174,7 +201,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def aaa(self, context, n: int = 10):
         if n and n <= 1987 and n > 0:
             await self.bot.say('***' + ('A' * n) + '***')
@@ -188,7 +215,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def ha(self, context, n: int = 10):
         if n and n <= 995 and n > 0:
             await self.bot.say('***' + ('HA' * n) + '***')
@@ -202,7 +229,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def triggered(self, context, n: int = 1):
         if n and n <= 31 and n > 0:
             await self.bot.say('***' + ('T̰͈ͪ̒̿R̼̘̔̆͜I̗̯̾ͨͣ͘G̾ͫ̍̾̂̊͛G͌̔ͤ҉̺͕̼E̐ͨ̉̾ͤͥͦR̼̘̎̂̐ͩ̏Ȩ̠̣͐̏̇̐D̤̟̦ͧ' * n) + '***')
@@ -217,7 +244,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def ooo(self, context, n: int = 10):
         if n and n <= 1987 and n > 0:
             await self.bot.say('***' + ('O' * n) + '***')
@@ -231,7 +258,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def happy(self, context, n: int = 10):
         happyemote = ':smile:'
         if n and n > 0 and n * len(happyemote) < 2000:
@@ -246,7 +273,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def ophiuchus(self, context, n: int = 10):
         emote = ':ophiuchus:'
         if n and n > 0 and n * len(emote) < 2000:
@@ -261,7 +288,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def ae(self, context, n: int = 10):
         chance = random.randint(1, 2)
         begintext = 'VA'
@@ -282,7 +309,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def mmm(self, context, n: int = 10):
         text = 'M'
         if n and n > 0 and (n * len(text)) + 6 < 2000:
@@ -297,11 +324,11 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.group(pass_context=True)
+    @commands.group()
     async def repeattext(self, context):
         return
 
-    @repeattext.command(pass_context=True)
+    @repeattext.command()
     async def regular(self, context, text, n: int = 10):
         if n > 0 and (n * len(text)) < 2000:
             await self.bot.say(text * n)
@@ -315,7 +342,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @repeattext.command(pass_context=True)
+    @repeattext.command()
     async def bold(self, context, text, n: int = 10):
         if n > 0 and (n * len(text)) + 4 < 2000:
             await self.bot.say('**' + text * n + '**')
@@ -329,7 +356,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @repeattext.command(pass_context=True)
+    @repeattext.command()
     async def italic(self, context, text, n: int = 10):
         if n > 0 and (n * len(text)) + 2 < 2000:
             await self.bot.say('*' + text * n + '*')
@@ -343,7 +370,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @repeattext.command(pass_context=True)
+    @repeattext.command()
     async def boldanditalic(self, context, text, n: int = 10):
         if n > 0 and (n * len(text)) + 6 < 2000:
             await self.bot.say('***' + text * n + '***')
@@ -357,7 +384,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def addstab(self, context, *, obj: str = None):
         if obj and obj not in self.stabbingobjects['objects']:
             obj = obj.rstrip(".");
@@ -379,7 +406,7 @@ class test:
             else:
                 await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def removestab(self, context, *, obj: str = None):
         if obj and obj in self.stabbingobjects['objects']:
             self.stabbingobjects['objects'].remove(obj)
@@ -388,7 +415,7 @@ class test:
         else:
             await self.bot.say('wat')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def liststab(self, context):
         objs = ''
         for obj in self.stabbingobjects['objects']:
@@ -400,21 +427,21 @@ class test:
         #	em.add_field(name=x + 1, value=self.stabbingobjects['objects'][x])
         await self.bot.say(embed=em)
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def eh(self, context, member: discord.Member = None):
         member = member or context.message.author
         await self.bot.say(member.mention + ', eh?')
 
-    @commands.group(pass_context=True, name='count')
-    async def _count(self, context):
+    @commands.group()
+    async def count(self, context):
         if context.invoked_subcommand is None:
             prefix = context.prefix
             await self.bot.say(
                 'Do \'{0}count start <start number> [end number] [mention]\' (end number and mention being optional, defaulting to 0 and false, respectively) to start or \'{0}count stop\' to stop any counting operation.'.format(
                     prefix))
 
-    @_count.command(pass_context=True, name='start')
-    async def _start(self, context, startnum: int = None, endnum: int = 0, mention: str = 'false'):
+    @_count.command()
+    async def start(self, context, startnum: int = None, endnum: int = 0, mention: str = 'false'):
         if self.counting == False:
             if startnum:
                 chance = random.randint(1, 10)
@@ -467,8 +494,8 @@ class test:
         else:
             await self.bot.say('I\'m already counting.')
 
-    @_count.command(pass_context=True, name='stop')
-    async def _stop(self, context):
+    @_count.command()
+    async def stop(self, context):
         self.counting = False
 
 
